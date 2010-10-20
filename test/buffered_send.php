5be5763d952a98a92b9c49e209c2a4bc775efeb5
Dmitry Prokudin <lhotski@gmail.com>
2010-10-20 10:55:26 +0400

    Buffered logging test added

--- test/buffered_send.php
+++ test/buffered_send.php
@@ -1,12 +1,14 @@
 <?php
 require(dirname(__FILE__)."/../conf.php");
 require(dirname(__FILE__)."/../Logbook.php");
+require(dirname(__FILE__)."/../Logbook/Buffer.php");
 
-$log = new Logbook(LOGBOOK_API_KEY);
+$logb = new Logbook_Buffer(LOGBOOK_API_KEY);
 
 foreach(Logbook::$SEVERITY_TYPES as $severity) {
-	$log->send($severity, "test", array(
+	$logb->add_entry($severity, "test", array(
 		"message" => "severity '$severity'"
 	));
 	echo "$severity added..\n";
 }
+$logb->flush();
